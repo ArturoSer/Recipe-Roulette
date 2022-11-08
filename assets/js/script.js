295a8b2a926f47f3375020fb28eba2bd9af97026
Arturo Serrato <arturo.serrato@yahoo.com>
2022-11-07 21:40:46 -0800

    added function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,46 @@
-var homeEl = document.querySelector('#homeDisplay');
-var displayEl = document.querySelector('#display');
-var startEl = document.querySelector('#start');
+var homeEl = document.getElementById('#homeDisplay');
+var displayEl = document.getElementById('#display');
+var startEl = document.getElementById('#start');
 
 // Fetching response from API 
-fetch('https://www.themealdb.com/api/json/v1/1/random.php').then((response) => {
-    return response.json()
-}).then((data) => {
-    console.log(data)
-});
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(res=>res.json())
+.then(data=> displayFoods(data.meals));
 
-// Adding event listener for when start button is clicked
+const displayFoods = foods=>{
+    const foodsDiv = document.createElement('food-items');
+
+    foods.forEach(meals=>{
+        const foodDiv = document.createElement('div');
+
+        foodDiv.className = 'meals';
+        const foodInfo = `
+        <h3>${meals.strMeal}</h3>
+        `;
+        foodDiv.innerHTML = foodInfo;
+        foodsDiv.appendChild(foodDiv);
+    
+        
+    });
+}
+
+
+
+//document.getElementById("start").addEventListener("click", homeEl);
+
+function createMeal(meals) {
+    const ingredients = [];
+    for (i=1; i<=20; i++) {
+        if(startEl[`strIngredients${i}`]) {
+            ingredients.push(
+                `${startEl[`strIngredients${i}`]} 
+                -${startEl[`strMeasure$`]}`
+            )
+            } else {
+             break;
+            }
+        }
+    }
 
 
 
