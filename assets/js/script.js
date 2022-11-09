14a857b44a85f4a61537efcb71223d908c9769c4
HunterHarvell <hunterharvell95@gmail.com>
2022-11-08 21:46:02 -0600

    add js for search functions and event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,32 @@
-var homeEl = document.getElementById('#homeDisplay');
-var displayEl = document.getElementById('#display');
-var startEl = document.getElementById('#start');
+const homeEl = document.querySelector('#homeDisplay');
+const displayEl = document.querySelector('#display');
+const randomBtn = document.querySelector('#rand-btn');
+const searchBtn = document.querySelector('#search-btn');
+
+// Random Meal Generator 
+function getRandom() {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
 
 // Fetching response from API 
-fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-.then(res=>res.json())
-.then(data=> displayFoods(data.meals));
+fetch('https://www.themealdb.com/api/json/v1/1/random.php').then((response) => {
+    return response.json()
+}).then((data) => {
+    console.log(data)
+});
 
-async function getRandomMeal() {
-    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-    const respData = await resp.json();
-    const randomMeal = respData.meals[0];
-    console.log(randomMeal);
+function searchMeal() {
+    fetch('www.themealdb.com/api/json/v1/1/search.php?s= VALUE')
+        .then(response => response.json())
+        .then(data => {
 
-    addMeal(randomMeal, true);
+        })
 }
 
-getRandomMeal();
+// calls for a random meal when the page is first loaded
+getRandom();
 
 
 // Adding event listener for when random button is clicked
+
+searchBtn.addEventListener('click', searchMeal);
+randomBtn.addEventListener('click', getRandom);
