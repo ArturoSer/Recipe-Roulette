129a427dfcbd40dada847939521f3916984613b3
HunterHarvell <hunterharvell95@gmail.com>
2022-11-12 11:13:54 -0600

    cleans up js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,17 @@ const randomBtn = document.querySelector("#rand-btn");
 const searchBtn = document.querySelector("#search-btn");
 
 // Fetching response from API
-fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-  .then((res) => res.json())
-  .then((data) => displayFoods(data.meals));
+function randomMeal () {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then((res) => res.json())
+        .then((data) => displayFoods(data.meals));
+}
+
+function searchMeal() {
+  fetch("www.themealdb.com/api/json/v1/1/search.php?s= VALUE")
+    .then((res) => res.json())
+    .then((data) => displayFoods(data.meals));
+}
 
 const displayFoods = (foods) => {
   const foodsDiv = document.createElement("food-items");
@@ -21,28 +29,8 @@ const displayFoods = (foods) => {
         `;
     foodDiv.innerHTML = foodInfo;
     foodsDiv.appendChild(foodDiv);
-  });
-};
-
-// Random Meal Generator
-function getRandom() {
-  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-    .then((response) => {
-      return response.json();
-    })
-
-    .then((data) => {
-      console.log(data);
-    });
+  })
 }
-
-function searchMeal() {
-  fetch("www.themealdb.com/api/json/v1/1/search.php?s= VALUE")
-    .then((response) => response.json())
-    .then((data) => {});
-}
-
-//document.getElementById("start").addEventListener("click", homeEl);
 
 function createMeal(meals) {
   const ingredients = [];
@@ -58,16 +46,10 @@ function createMeal(meals) {
   }
 }
 
-function searchMeal() {
-  fetch("www.themealdb.com/api/json/v1/1/search.php?s= VALUE")
-    .then((response) => response.json())
-    .then((data) => {});
-}
-
 // calls for a random meal when the page is first loaded
 getRandom();
 
 // Adding event listener for when random button is clicked
 
 searchBtn.addEventListener("click", searchMeal);
-randomBtn.addEventListener("click", getRandom);
+randomBtn.addEventListener("click", randomMeal);
